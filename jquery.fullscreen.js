7db29bd10a40c754b3b623e7ac7b18aa3b489867
Rui Lopes <ruidlopes@gmail.com>
2011-08-13 00:01:31 +0100

    initial import of ppk's browser detection code

--- jquery.fullscreen.js
+++ jquery.fullscreen.js
@@ -1,5 +1,59 @@
 (function($, window, documentElement, height, width) {
     
+    // browser detection code courtesy of quirksmode, http://www.quirksmode.org/js/detect.html
+    // slightly simplified, as well as minor changes for readability purposes
+
+    var BrowserDetect = {
+        init: function () {
+            this.browser = this.searchString(this.dataBrowser) || "An unknown browser";
+            this.version = this.searchVersion(navigator.userAgent)
+                || this.searchVersion(navigator.appVersion)
+                || "an unknown version";
+            this.OS = this.searchString(this.dataOS) || "an unknown OS";
+        },
+
+        searchString: function (data) {
+            for (var i=0;i<data.length;i++)    {
+                var dataString = data[i].string;
+                var dataProp = data[i].prop;
+                this.versionSearchString = data[i].versionSearch || data[i].identity;
+                if (dataString) {
+                    if (dataString.indexOf(data[i].subString) != -1)
+                        return data[i].identity;
+                }
+                else if (dataProp)
+                    return data[i].identity;
+            }
+        },
+
+        searchVersion: function (dataString) {
+            var index = dataString.indexOf(this.versionSearchString);
+            if (index == -1) return;
+            return parseFloat(dataString.substring(index+this.versionSearchString.length+1));
+        },
+
+        dataBrowser: [
+            { string: navigator.userAgent, subString: "Chrome",  identity: "Chrome"                            },
+            { string: navigator.vendor,    subString: "Apple",   identity: "Safari",  versionSearch: "Version" },
+            { prop: window.opera,                                identity: "Opera",   versionSearch: "Version" },
+            { string: navigator.userAgent, subString: "Firefox", identity: "Firefox"                           },
+            { string: navigator.userAgent, subString: "MSIE",    identity: "Explorer", versionSearch: "MSIE"   }
+        ],
+
+        dataOS : [
+            { string: navigator.platform,  subString: "Win",    identity: "Windows"     },
+            { string: navigator.platform,  subString: "Mac",    identity: "Mac"         },
+            { string: navigator.userAgent, subString: "iPhone", identity: "iPhone/iPod" },
+            { string: navigator.platform,  subString: "Linux",  identity: "Linux"       }
+        ]
+
+    };
+
+    BrowserDetect.init();
+    // Browser name: BrowserDetect.browser
+    // Browser version: BrowserDetect.version
+    // OS name: BrowserDetect.OS
+
     var 
         isFullScreen = function() {
             return (documentElement.clientHeight == height && documentElement.clientWidth == width) ||
