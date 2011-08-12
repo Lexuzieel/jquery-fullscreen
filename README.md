ee02ef2237decec24493037c498d6c6ff1201c39
Rui Lopes <ruidlopes@gmail.com>
2011-08-12 23:44:22 +0100

    whitespace fixups

--- README.md
+++ README.md
@@ -6,26 +6,26 @@ This plugin provides a technique for detecting if a browser is in fullscreen mod
 
 Add `jQuery` and `jquery.fullscreen.js` to your HTML document. No explicit initialization is required, this plugin handles that automatically. Afterwards, bind a fullscreen event to the `window` object. Example:
 
-	$(window).bind("fullscreen-on", function(e) {
-		console.log("we're on fullscreen now");
-	});
+    $(window).bind("fullscreen-on", function(e) {
+        console.log("we're on fullscreen now");
+    });
 
 The following events are provided:
 
 * `fullscreen-toggle` is triggered whenever the fullscreen state changes. The event handling function takes the following form:
-		
-		$(window).bind("fullscreen-toggle", function(e, state) {
-			...
-		});
+        
+        $(window).bind("fullscreen-toggle", function(e, state) {
+            ...
+        });
 
-	where `state` represents the fullscreen state as a `boolean` value;
+    where `state` represents the fullscreen state as a `boolean` value;
 
 * `fullscreen-on` is triggered when the browser enters fullscreen;
 * `fullscreen-off` is triggered when the browser exits fullscreen.
 
 If you need to check the current fullscreen state, grab the `fullscreen-state` data property from the `window` object, as follows:
 
-	var isFullScreen = $(window).data('fullscreen-state');
+    var isFullScreen = $(window).data('fullscreen-state');
 
 
 The `example.html` document details examples for all events.
--- jquery.fullscreen.js
+++ jquery.fullscreen.js
@@ -1,35 +1,35 @@
 (function($, window, documentElement, height, width) {
-	
-	var 
-		isFullScreen = function() {
-			return (documentElement.clientHeight == height && documentElement.clientWidth == width) ||
-				window.fullScreen ||
-				(window.outerHeight == height && window.outerWidth == width)
-			;
-		}
-		,$window = $(window)
-	;
-	
-	$window
-		.data('fullscreen-state', isFullScreen())
-		.resize(function() {
-			var fullscreenState = isFullScreen();
-			
-			if ($window.data('fullscreen-state') && !fullscreenState) {
-				$window
-					.data('fullscreen-state', fullscreenState)
-					.trigger('fullscreen-toggle', [false])
-					.trigger('fullscreen-off')
-				;
-			}
-			else if (!$window.data('fullscreen-state') && fullscreenState) {
-				$window
-					.data('fullscreen-state', fullscreenState)
-					.trigger('fullscreen-toggle', [true])
-					.trigger('fullscreen-on')
-				;
-			}
-		})
-	;
+    
+    var 
+        isFullScreen = function() {
+            return (documentElement.clientHeight == height && documentElement.clientWidth == width) ||
+                window.fullScreen ||
+                (window.outerHeight == height && window.outerWidth == width)
+            ;
+        }
+        ,$window = $(window)
+    ;
+    
+    $window
+        .data('fullscreen-state', isFullScreen())
+        .resize(function() {
+            var fullscreenState = isFullScreen();
+            
+            if ($window.data('fullscreen-state') && !fullscreenState) {
+                $window
+                    .data('fullscreen-state', fullscreenState)
+                    .trigger('fullscreen-toggle', [false])
+                    .trigger('fullscreen-off')
+                ;
+            }
+            else if (!$window.data('fullscreen-state') && fullscreenState) {
+                $window
+                    .data('fullscreen-state', fullscreenState)
+                    .trigger('fullscreen-toggle', [true])
+                    .trigger('fullscreen-on')
+                ;
+            }
+        })
+    ;
 
 })(jQuery, this, document.documentElement, screen.height, screen.width);
